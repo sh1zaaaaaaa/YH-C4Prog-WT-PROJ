03b23aff8a60e133732fb286bbf6f317d33a5aa7
Yehor Hiba <20128413@tafe.wa.edu.au>
2024-12-01 22:09:56 +0800

    feat: Add home page.

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./index.html",
+      "./src/**/*.{html,js,ts,tsx}",
+  ],
+  theme: {
+      screens: {
+      sm: '640px', // Small devices
+      md: '768px', // Medium devices
+      lg: '1024px', // Large devices
+      xl: '1280px', // Extra large devices
+      },
+    extend: {
+        fontFamily: {
+            sans: ['Arial', 'sans-serif']
+        },
+    },
+  },
+  plugins: [],
+}
+
